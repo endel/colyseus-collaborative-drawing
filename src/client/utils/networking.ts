a44c481b9855c3b9d2aaad2ce4b126ce48d0ca81
Endel Dreyer <endel.dreyer@gmail.com>
2019-08-28 15:47:49 -0300

    fixes wss protocol on heroku.

--- src/client/utils/networking.ts
+++ src/client/utils/networking.ts
@@ -1,4 +1,6 @@
 import { Client } from "colyseus.js";
 
+const protocol = location.protocol.replace("http", "ws");
 const port = (location.port && `:${location.port}`);
-export const client = new Client(`ws://${location.hostname}${port}`);
+const host = location.hostname;
+export const client = new Client(`${protocol}//${host}${port}`);
